81d2eba2b782c621f32ffbd4c74a8752dc326c4d
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2021-03-19 00:11:40 +0100

    Small changes to live scripts and new training script for smalle obs space

--- diambraInterface/customPolicies/customCnnPolicy.py
+++ diambraInterface/customPolicies/customCnnPolicy.py
@@ -1,5 +1,20 @@
 from stable_baselines.common.policies import *
 
+def local_nature_cnn_small(scaled_images, **kwargs):
+    """
+    CNN from Nature paper.
+
+    :param scaled_images: (TensorFlow Tensor) Image input placeholder
+    :param kwargs: (dict) Extra keywords parameters for the convolutional layers of the CNN
+    :return: (TensorFlow Tensor) The CNN output layer
+    """
+    activ = tf.nn.relu
+    layer_1 = activ(conv(scaled_images, 'c1', n_filters=32, filter_size=8, stride=4, init_scale=np.sqrt(2), **kwargs))
+    layer_2 = activ(conv(layer_1, 'c2', n_filters=64, filter_size=4, stride=2, init_scale=np.sqrt(2), **kwargs))
+    layer_3 = activ(conv(layer_2, 'c3', n_filters=64, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
+    layer_3 = conv_to_fc(layer_3)
+    return activ(linear(layer_3, 'fc1', n_hidden=256, init_scale=np.sqrt(2)))
+
 class CustCnnPolicy(ActorCriticPolicy):
     """
     Policy object that implements actor critic, using a feed forward neural network.
