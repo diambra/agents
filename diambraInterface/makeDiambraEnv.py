4dff333fdeb91dfd5d2d5cd4d94c48338a6c8781
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2021-05-17 13:53:41 +0200

    Working diambraWrappers and its test

--- diambraInterface/makeDiambraEnv.py
+++ diambraInterface/makeDiambraEnv.py
@@ -1,21 +1,21 @@
 from diambraWrappers import *
 
-def make_diambra_env(diambraMame, env_prefix, seed, diambra_kwargs, diambra_gym_kwargs,
-                     wrapper_kwargs=None, traj_rec_kwargs=None, key_to_add=None):
+def makeDiambraEnv(diambraMame, envPrefix, seed, diambraKwargs, diambraGymKwargs,
+                     wrapperKwargs=None, trajRecKwargs=None, keyToAdd=None):
     """
     Create a wrapped, monitored VecEnv for Atari.
     :param diambraMame: (class) DIAMBRAGym interface class
     :param seed: (int) the initial seed for RNG
-    :param wrapper_kwargs: (dict) the parameters for wrap_deepmind function
+    :param wrapperKwargs: (dict) the parameters for wrapDeepmind function
     """
-    if wrapper_kwargs is None:
-        wrapper_kwargs = {}
+    if wrapperKwargs is None:
+        wrapperKwargs = {}
 
-    env = make_diambra(diambraMame, env_prefix, diambra_kwargs, diambra_gym_kwargs)
+    env = makeDiambra(diambraMame, envPrefix, diambraKwargs, diambraGymKwargs)
     env.seed(seed)
-    env = wrap_deepmind(env, **wrapper_kwargs)
-    env = additional_obs(env, key_to_add)
-    if type(traj_rec_kwargs) != type(None):
-        env = TrajectoryRecorder(env, **traj_rec_kwargs, key_to_add=key_to_add)
+    env = wrapDeepmind(env, **wrapperKwargs)
+    env = additionalObs(env, keyToAdd)
+    if type(trajRecKwargs) != type(None):
+        env = TrajectoryRecorder(env, **trajRecKwargs, keyToAdd=keyToAdd)
 
     return env
