bde9cb58d94599521c85f6a8055ac950f9321e7f
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2021-09-27 13:46:44 +0200

    Added new speed test for Stable Baselines

---
+++ tests/speedTestSBTraining.py
@@ -0,0 +1,115 @@
+import sys, os, time
+import argparse
+
+if __name__ == '__main__':
+    timeDepSeed = int((time.time()-int(time.time()-0.5))*1000)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--romsPath',    type=str,  required=True, help='Roms Path')
+    opt = parser.parse_args()
+    print(opt)
+
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(base_path, '../'))
+
+    from makeStableBaselinesEnv import makeStableBaselinesEnv
+
+    import tensorflow as tf
+
+    from sbUtils import linear_schedule, AutoSave, modelCfgSave
+    from customPolicies.customCnnPolicy import CustCnnPolicy, local_nature_cnn_small
+
+    from stable_baselines import PPO2
+
+    # Common settings
+    diambraKwargs = {}
+    diambraKwargs["gameId"]   = "doapp"
+    diambraKwargs["romsPath"] = opt.romsPath
+
+    diambraKwargs["stepRatio"] = 6
+    diambraKwargs["lockFps"]   = False
+    diambraKwargs["render"]    = False
+
+    diambraKwargs["player"] = "Random" # P1 / P2
+
+    diambraKwargs["characters"] =[["Random", "Random"], ["Random", "Random"]]
+
+    diambraKwargs["difficulty"]  = 3
+    diambraKwargs["charOutfits"] =[2, 2]
+
+    # DIAMBRA gym kwargs
+    diambraGymKwargs = {}
+    diambraGymKwargs["actionSpace"] = "discrete"
+    diambraGymKwargs["attackButCombinations"] = False
+
+    # Env wrappers kwargs
+    wrapperKwargs = {}
+    wrapperKwargs["noOpMax"] = 0
+    wrapperKwargs["hwcObsResize"] = [128, 128, 1]
+    wrapperKwargs["normalizeRewards"] = True
+    wrapperKwargs["clipRewards"] = False
+    wrapperKwargs["frameStack"] = 4
+    wrapperKwargs["dilation"] = 1
+    wrapperKwargs["actionsStack"] = 12
+    wrapperKwargs["scale"] = True
+    wrapperKwargs["scaleMod"] = 0
+
+    # Additional obs key list
+    keyToAdd = []
+    keyToAdd.append("actions")
+    keyToAdd.append("ownHealth")
+    keyToAdd.append("oppHealth")
+    keyToAdd.append("ownSide")
+    keyToAdd.append("oppSide")
+    keyToAdd.append("stage")
+
+    keyToAdd.append("ownChar")
+    keyToAdd.append("oppChar")
+
+    numEnv=8
+
+    envId = "doapp_Train"
+    env = makeStableBaselinesEnv(envId, numEnv, timeDepSeed, diambraKwargs, diambraGymKwargs,
+                                 wrapperKwargs, keyToAdd=keyToAdd, useSubprocess=True)
+
+    # Policy param
+    nActions      = env.get_attr("nActions")[0][0]
+    nActionsStack = env.get_attr("nActionsStack")[0]
+    nChar         = env.get_attr("numberOfCharacters")[0]
+    charNames     = env.get_attr("charNames")[0]
+
+    policyKwargs={}
+    policyKwargs["n_add_info"] = nActionsStack*(nActions[0]+nActions[1]) + len(keyToAdd)-3 + 2*nChar
+    policyKwargs["layers"] = [64, 64]
+
+    policyKwargs["cnn_extractor"] = local_nature_cnn_small
+
+    print("nActions =", nActions)
+    print("nChar =", nChar)
+    print("nAddInfo =", policyKwargs["n_add_info"])
+
+    # PPO param
+    setGamma = 0.94
+    setLearningRate = linear_schedule(2.5e-4, 2.5e-6)
+    setClipRange = linear_schedule(0.15, 0.025)
+    setClipRangeVf = setClipRange
+    nSteps = 128
+
+    # Initialize the model
+    model = PPO2(CustCnnPolicy, env, verbose=1,
+                 gamma = setGamma, nminibatches=4, noptepochs=4, n_steps=nSteps,
+                 learning_rate=setLearningRate, cliprange=setClipRange, cliprange_vf=setClipRangeVf,
+                 policy_kwargs=policyKwargs)
+
+    print("Model discount factor = ", model.gamma)
+
+    # Train the agent
+    timeSteps = nSteps*2*numEnv
+    tic = time.time()
+    model.learn(total_timesteps=timeSteps)
+    toc = time.time()
+
+    print("Time elapsed = ", toc-tic)
+
+    # Close the environment
+    env.close()
