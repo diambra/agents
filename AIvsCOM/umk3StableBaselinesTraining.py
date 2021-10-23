2f3c067891ec663710cbf037fd582d69a2e53796
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2021-10-24 00:58:24 +0200

    Fixed SB training scripts

--- AIvsCOM/umk3StableBaselinesTraining.py
+++ AIvsCOM/umk3StableBaselinesTraining.py
@@ -20,42 +20,40 @@ if __name__ == '__main__':
 
     from stable_baselines import PPO2
 
-    # Common settings
-    diambraKwargs = {}
-    diambraKwargs["gameId"]   = "umk3"
-    diambraKwargs["romsPath"] = os.path.join(base_path, "../../roms/mame/")
+    # Settings
+    settings = {}
+    settings["gameId"]   = "umk3"
+    settings["romsPath"] = os.path.join(base_path, "../../roms/mame/")
 
-    diambraKwargs["stepRatio"] = 6
-    diambraKwargs["lockFps"] = False
-    diambraKwargs["render"]  = False
+    settings["stepRatio"] = 6
+    settings["lockFps"] = False
+    settings["render"]  = False
 
-    diambraKwargs["player"] = "Random" # P1 / P2
+    settings["player"] = "Random" # P1 / P2
 
-    diambraKwargs["characters"] =[["Sektor", "Random"], ["Sektor", "Random"]]
+    settings["characters"] =[["Sektor", "Random"], ["Sektor", "Random"]]
 
-    diambraKwargs["difficulty"]  = 4
-    diambraKwargs["tower"]  = 3
-    diambraKwargs["charOutfits"] =[2, 2]
+    settings["difficulty"]  = 4
+    settings["tower"]  = 3
+    settings["charOutfits"] =[2, 2]
 
-    diambraKwargs["continueGame"] = 0.0
-    diambraKwargs["showFinal"] = False
+    settings["continueGame"] = 0.0
+    settings["showFinal"] = False
 
-    # DIAMBRA gym kwargs
-    diambraGymKwargs = {}
-    diambraGymKwargs["actionSpace"] = "discrete"
-    diambraGymKwargs["attackButCombinations"] = False
+    settings["actionSpace"] = "discrete"
+    settings["attackButCombinations"] = False
 
-    # Env wrappers kwargs
-    wrapperKwargs = {}
-    wrapperKwargs["noOpMax"] = 0
-    wrapperKwargs["hwcObsResize"] = [128, 128, 1]
-    wrapperKwargs["normalizeRewards"] = True
-    wrapperKwargs["clipRewards"] = False
-    wrapperKwargs["frameStack"] = 4
-    wrapperKwargs["dilation"] = 1
-    wrapperKwargs["actionsStack"] = 12
-    wrapperKwargs["scale"] = True
-    wrapperKwargs["scaleMod"] = 0
+    # Wrappers Settings
+    wrappersSettings = {}
+    wrappersSettings["noOpMax"] = 0
+    wrappersSettings["hwcObsResize"] = [128, 128, 1]
+    wrappersSettings["normalizeRewards"] = True
+    wrappersSettings["clipRewards"] = False
+    wrappersSettings["frameStack"] = 4
+    wrappersSettings["dilation"] = 1
+    wrappersSettings["actionsStack"] = 12
+    wrappersSettings["scale"] = True
+    wrappersSettings["scaleMod"] = 0
 
     # Additional obs key list
     keyToAdd = []
@@ -76,8 +74,8 @@ if __name__ == '__main__':
     numEnv=16
 
     envId = "umk3_Train"
-    env = makeStableBaselinesEnv(envId, numEnv, timeDepSeed, diambraKwargs, diambraGymKwargs,
-                                 wrapperKwargs, keyToAdd=keyToAdd, useSubprocess=True)
+    env = makeStableBaselinesEnv(envId, numEnv, timeDepSeed, settings,
+                                 wrappersSettings, keyToAdd=keyToAdd, useSubprocess=True)
 
     print("Obs_space = ", env.observation_space)
     print("Obs_space type = ", env.observation_space.dtype)
@@ -86,7 +84,7 @@ if __name__ == '__main__':
 
     print("Act_space = ", env.action_space)
     print("Act_space type = ", env.action_space.dtype)
-    if diambraGymKwargs["actionSpace"] == "multiDiscrete":
+    if settings["actionSpace"] == "multiDiscrete":
         print("Act_space n = ", env.action_space.nvec)
     else:
         print("Act_space n = ", env.action_space.n)
@@ -146,7 +144,7 @@ if __name__ == '__main__':
     model.save(modelPath)
     # Save the correspondent CFG file
     modelCfgSave(modelPath, "PPOSmall", nActions, charNames,
-                 diambraKwargs, diambraGymKwargs, wrapperKwargs, keyToAdd)
+                 settings, wrappersSettings, keyToAdd)
 
     # Close the environment
     env.close()
