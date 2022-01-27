95bd07f30d9830e826da4a4180aa12998b1efc77
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2022-01-27 22:54:35 +0000

    V1.0

--- tests/testStableBaselinesTraining.py
+++ tests/testStableBaselinesTraining.py
@@ -6,15 +6,18 @@ if __name__ == '__main__':
 
     try:
         parser = argparse.ArgumentParser()
-        parser.add_argument('--gameId', type=str, default="doapp", help='Game ID')
+        parser.add_argument('--gameId',    type=str,  default="doapp", help='Game ID')
+        parser.add_argument('--nEnvs',     type=int,  default=2,       help='Number of parallel envs')
+        parser.add_argument('--stepRatio', type=int,  default=6,       help='Step ratio')
+        parser.add_argument('--lockFps',   type=bool, default=False,   help='Lock Fps')
+        parser.add_argument('--render',    type=bool, default=False,   help='Rendering env')
         opt = parser.parse_args()
         print(opt)
 
         base_path = os.path.dirname(os.path.abspath(__file__))
         sys.path.append(os.path.join(base_path, '../'))
-        sys.path.append(os.path.join(base_path, '../../games_cpp/gym/'))
 
-        modelFolder = "./{}StableBaselinesTestModel/".format(opt.gameId)
+        modelFolder = os.path.join(base_path, "{}StableBaselinesTestModel/".format(opt.gameId))
 
         os.makedirs(modelFolder, exist_ok=True)
 
@@ -22,80 +25,70 @@ if __name__ == '__main__':
 
         import tensorflow as tf
 
-        from sbUtils import linear_schedule, AutoSave
+        from sbUtils import linear_schedule, AutoSave, modelCfgSave
         from customPolicies.customCnnPolicy import CustCnnPolicy, local_nature_cnn_small
 
         from stable_baselines import PPO2
 
         # Common settings
-        diambraKwargs = {}
-        diambraKwargs["gameId"]   = opt.gameId
-        diambraKwargs["romsPath"] = os.path.join(base_path, "../../roms/mame/")
+        settings = {}
+        settings["gameId"]   = opt.gameId
+        settings["romsPath"] = os.path.join(base_path, "../../roms/mame/")
 
-        diambraKwargs["mamePath"] = os.path.join(base_path, "../../customMAME/")
-        diambraKwargs["libPath"] = os.path.join(base_path, "../../games_cpp/build/diambraEnvLib/libdiambraEnv.so")
+        settings["stepRatio"] = opt.stepRatio
+        settings["lockFps"]   = opt.lockFps
+        settings["render"]    = opt.render
 
-        diambraKwargs["mameDiambraStepRatio"] = 6
-        diambraKwargs["lockFps"] = False
-        diambraKwargs["render"]     = False
+        settings["player"] = "Random" # P1 / P2
 
-        diambraKwargs["player"] = "Random" # P1 / P2
+        settings["characters"] =[["Random", "Random", "Random"], ["Random", "Random", "Random"]]
 
-        diambraKwargs["characters"] =[["Random", "Random"], ["Random", "Random"]]
-
-        diambraKwargs["difficulty"]  = 3
-        diambraKwargs["charOutfits"] =[2, 2]
+        settings["difficulty"]  = 3
+        settings["charOutfits"] =[2, 2]
 
         # DIAMBRA gym kwargs
-        diambraGymKwargs = {}
-        diambraGymKwargs["actionSpace"] = "discrete"
-        diambraGymKwargs["attackButCombinations"] = False
+        settings["actionSpace"] = "discrete"
+        settings["attackButCombination"] = False
 
         # Env wrappers kwargs
-        wrapperKwargs = {}
-        wrapperKwargs["noOpMax"] = 0
-        wrapperKwargs["hwcObsResize"] = [128, 128, 1]
-        wrapperKwargs["normalizeRewards"] = True
-        wrapperKwargs["clipRewards"] = False
-        wrapperKwargs["frameStack"] = 4
-        wrapperKwargs["dilation"] = 1
-        wrapperKwargs["actionsStack"] = 12
-        wrapperKwargs["scale"] = True
-        wrapperKwargs["scaleMod"] = 0
+        wrappersSettings = {}
+        wrappersSettings["noOpMax"] = 0
+        wrappersSettings["hwcObsResize"] = [128, 128, 1]
+        wrappersSettings["normalizeRewards"] = True
+        wrappersSettings["clipRewards"] = False
+        wrappersSettings["frameStack"] = 4
+        wrappersSettings["dilation"] = 1
+        wrappersSettings["actionsStack"] = 12
+        wrappersSettings["scale"] = True
+        wrappersSettings["scaleMod"] = 0
 
         # Additional obs key list
         keyToAdd = []
-        keyToAdd.append("actions") # wrapperKwargs["actionsStack"]*(env.n_actions[0]+env.n_actions[1])
+        keyToAdd.append("actions")
 
         if opt.gameId != "tektagt":
-            keyToAdd.append("ownHealth")   # 1
-            keyToAdd.append("oppHealth")   # 1
+            keyToAdd.append("ownHealth")
+            keyToAdd.append("oppHealth")
         else:
-            keyToAdd.append("ownHealth1") # 1
-            keyToAdd.append("ownHealth2") # 1
-            keyToAdd.append("oppHealth1") # 1
-            keyToAdd.append("oppHealth2") # 1
-            keyToAdd.append("ownActiveChar") # 1
-            keyToAdd.append("oppActiveChar") # 1
+            keyToAdd.append("ownHealth1")
+            keyToAdd.append("ownHealth2")
+            keyToAdd.append("oppHealth1")
+            keyToAdd.append("oppHealth2")
+            keyToAdd.append("ownActiveChar")
+            keyToAdd.append("oppActiveChar")
 
-        keyToAdd.append("ownPosition")     # 1
-        keyToAdd.append("oppPosition")     # 1
-        keyToAdd.append("stage")           # 1
+        keyToAdd.append("ownSide")
+        keyToAdd.append("oppSide")
+        keyToAdd.append("stage")
 
-        if opt.gameId != "tektagt":
-            keyToAdd.append("ownChar") # len(env.charNames)
-            keyToAdd.append("oppChar") # len(env.charNames)
-        else:
-            keyToAdd.append("ownChar1") # len(env.charNames)
-            keyToAdd.append("ownChar2") # len(env.charNames)
-            keyToAdd.append("oppChar1") # len(env.charNames)
-            keyToAdd.append("oppChar2") # len(env.charNames)
+        keyToAdd.append("ownChar")
+        keyToAdd.append("oppChar")
 
-        numEnv=2
+        numEnv=opt.nEnvs
 
         envId = opt.gameId + "_Train"
-        env = makeStableBaselinesEnv(envId, numEnv, timeDepSeed, diambraKwargs, diambraGymKwargs,
-                                     wrapperKwargs, keyToAdd=keyToAdd, useSubprocess=True)
+        env = makeStableBaselinesEnv(envId, numEnv, timeDepSeed, settings,
+                                     wrappersSettings, keyToAdd=keyToAdd, useSubprocess=True)
 
         print("Obs_space = ", env.observation_space)
         print("Obs_space type = ", env.observation_space.dtype)
@@ -104,15 +97,16 @@ if __name__ == '__main__':
 
         print("Act_space = ", env.action_space)
         print("Act_space type = ", env.action_space.dtype)
-        if diambraGymKwargs["actionSpace"] == "multiDiscrete":
+        if settings["actionSpace"] == "multiDiscrete":
             print("Act_space n = ", env.action_space.nvec)
         else:
             print("Act_space n = ", env.action_space.n)
 
         # Policy param
-        nActions = env.get_attr("nActions")[0][0]
+        nActions      = env.get_attr("nActions")[0][0]
         nActionsStack = env.get_attr("nActionsStack")[0]
-        nChar = env.get_attr("numberOfCharacters")[0]
+        nChar         = env.get_attr("numberOfCharacters")[0]
+        charNames     = env.get_attr("charNames")[0]
 
         policyKwargs={}
         policyKwargs["n_add_info"] = nActionsStack*(nActions[0]+nActions[1]) + len(keyToAdd)-3 + 2*nChar
@@ -129,25 +123,30 @@ if __name__ == '__main__':
         setLearningRate = linear_schedule(2.5e-4, 2.5e-6)
         setClipRange = linear_schedule(0.15, 0.025)
         setClipRangeVf = setClipRange
+        nSteps = 128
 
         # Initialize the model
         model = PPO2(CustCnnPolicy, env, verbose=1,
-                     gamma = setGamma, nminibatches=4, noptepochs=4, n_steps=128,
+                     gamma = setGamma, nminibatches=4, noptepochs=4, n_steps=nSteps,
                      learning_rate=setLearningRate, cliprange=setClipRange, cliprange_vf=setClipRangeVf,
                      policy_kwargs=policyKwargs)
 
         print("Model discount factor = ", model.gamma)
 
         # Create the callback: autosave every USER DEF steps
-        autoSaveCallback = AutoSave(check_freq=256, numEnv=numEnv,
-                                    save_path=os.path.join(modelFolder, str("_".join(keyToAdd))+"_0M_"))
+        autoSaveCallback = AutoSave(check_freq=nSteps*opt.nEnvs, numEnv=numEnv,
+                                    save_path=os.path.join(modelFolder, "0M_"))
 
         # Train the agent
-        timeSteps = 512
+        timeSteps = nSteps*2*opt.nEnvs
         model.learn(total_timesteps=timeSteps, callback=autoSaveCallback)
 
         # Save the agent
-        model.save(os.path.join(modelFolder, str("_".join(keyToAdd))+"_512"))
+        modelPath = os.path.join(modelFolder, str(timeSteps))
+        model.save(modelPath)
+        # Save the correspondent CFG file
+        modelCfgSave(modelPath, "PPOSmall", nActions, charNames,
+                     settings, wrappersSettings, keyToAdd)
 
         # Close the environment
         env.close()
