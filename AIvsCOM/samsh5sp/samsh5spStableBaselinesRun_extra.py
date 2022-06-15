903d347d1b8dd96b3a29137b399a603fc3e84c60
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2022-06-16 00:40:07 +0200

    updated SB training scripts

--- AIvsCOM/samsh5sp/samsh5spStableBaselinesRun_extra.py
+++ AIvsCOM/samsh5sp/samsh5spStableBaselinesRun_extra.py
@@ -18,12 +18,8 @@ if __name__ == '__main__':
     # Settings
     settings = {}
     settings["gameId"]   = "samsh5sp"
-    settings["romsPath"] = os.path.join(base_path, "../../../roms/mame/")
-
     settings["stepRatio"] = 6
-    settings["lockFps"] = False
-    settings["render"]  = True
-
+    settings["frameShape"] = [128, 128, 1]
     settings["player"] = "P1" # P1 / P2
 
     settings["characters"] =[["Haohmaru"], ["Haohmaru"]]
@@ -40,8 +36,7 @@ if __name__ == '__main__':
     # Wrappers settings
     wrappersSettings = {}
     wrappersSettings["noOpMax"] = 0
-    wrappersSettings["hwcObsResize"] = [128, 128, 1]
-    wrappersSettings["normalizeRewards"] = True
+    wrappersSettings["rewardNormalization"] = True
     wrappersSettings["clipRewards"] = False
     wrappersSettings["frameStack"] = 4
     wrappersSettings["dilation"] = 1
@@ -60,11 +55,8 @@ if __name__ == '__main__':
     keyToAdd.append("oppSide")
     keyToAdd.append("stage")
 
-    numEnv=1
-
-    envId = "samsh5sp_Train"
-    env = makeStableBaselinesEnv(envId, numEnv, timeDepSeed, settings,
-                                 wrappersSettings, keyToAdd=keyToAdd, noVec=True)
+    env, numEnv = makeStableBaselinesEnv(timeDepSeed, settings, wrappersSettings,
+                                         keyToAdd=keyToAdd, noVec=True)
 
     # Load the trained agent
     model = PPO2.load(os.path.join(modelFolder, "293M"))
--- AIvsCOM/samsh5sp/samsh5spStableBaselinesTraining_extra.py
+++ AIvsCOM/samsh5sp/samsh5spStableBaselinesTraining_extra.py
@@ -23,12 +23,8 @@ if __name__ == '__main__':
     # Settings
     settings = {}
     settings["gameId"]   = "samsh5sp"
-    settings["romsPath"] = os.path.join(base_path, "../../../roms/mame/")
-
     settings["stepRatio"] = 6
-    settings["lockFps"] = False
-    settings["render"]  = False
-
+    settings["frameShape"] = [128, 128, 1]
     settings["player"] = "Random" # P1 / P2
 
     settings["characters"] =[["Haohmaru"], ["Haohmaru"]]
@@ -45,8 +41,7 @@ if __name__ == '__main__':
     # Wrappers settings
     wrappersSettings = {}
     wrappersSettings["noOpMax"] = 0
-    wrappersSettings["hwcObsResize"] = [128, 128, 1]
-    wrappersSettings["normalizeRewards"] = True
+    wrappersSettings["rewardNormalization"] = True
     wrappersSettings["clipRewards"] = False
     wrappersSettings["frameStack"] = 4
     wrappersSettings["dilation"] = 1
@@ -65,11 +60,8 @@ if __name__ == '__main__':
     keyToAdd.append("oppSide")
     keyToAdd.append("stage")
 
-    numEnv=16
-
-    envId = "samsh5sp_Train"
-    env = makeStableBaselinesEnv(envId, numEnv, timeDepSeed, settings,
-                                 wrappersSettings, keyToAdd=keyToAdd, useSubprocess=True)
+    env, numEnv = makeStableBaselinesEnv(timeDepSeed, settings, wrappersSettings,
+                                         keyToAdd=keyToAdd, useSubprocess=True)
 
     print("Obs_space = ", env.observation_space)
     print("Obs_space type = ", env.observation_space.dtype)
@@ -101,7 +93,7 @@ if __name__ == '__main__':
 
     # PPO param
     setGamma = 0.94
-    modelCheckpoint = "0M"
+    modelCheckpoint = "40M"
     setLearningRate = linear_schedule(2.5e-4, 2.5e-6)
     setClipRange = linear_schedule(0.15, 0.025)
     setClipRangeVf = setClipRange
@@ -134,7 +126,7 @@ if __name__ == '__main__':
     model.learn(total_timesteps=timeSteps, callback=autoSaveCallback)
 
     # Save the agent
-    modelPath = os.path.join(modelFolder, "40M")
+    modelPath = os.path.join(modelFolder, "80M")
     model.save(modelPath)
     # Save the correspondent CFG file
     modelCfgSave(modelPath, "PPOSmall", nActions, charNames,
