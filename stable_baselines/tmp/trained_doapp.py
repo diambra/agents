c2850be0f973712d5713eab964deb05643f305fa
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2022-08-28 20:17:22 +0200

    restructuring repo unifying training scripts

--- stable_baselines/tmp/trained_doapp.py
+++ stable_baselines/tmp/trained_doapp.py
@@ -6,60 +6,62 @@ sys.path.append(os.path.join(base_path, '../.'))
 from make_stable_baselines_env import make_stable_baselines_env
 from stable_baselines import PPO2
 
-time_dep_seed = int((time.time() - int(time.time() - 0.5)) * 1000)
+if __name__ == "__main__":
 
-model_folder = os.path.join(base_path, "models/")
+    time_dep_seed = int((time.time() - int(time.time() - 0.5)) * 1000)
 
-# Settings
-settings = {
-    "game_id": "doapp",
-    "env_address": "0.0.0.0:50052",
-    "player": "Random",
-    "step_ratio": 6,
-    "frame_shape": [128, 128, 1],
-    "hardcore": False,
-    "difficulty": 4,
-    "characters": [["Kasumi"], ["Kasumi"]],
-    "char_outfits": [2, 2],
-    "action_space": "discrete",
-    "attack_but_combination": False
-}
+    model_folder = os.path.join(base_path, "models/")
 
-# Wrappers Settings
-wrappers_settings = {
-    "reward_normalization": True,
-    "frame_stack": 4,
-    "actions_stack": 12,
-    "scale": True
-}
+    # Settings
+    settings = {
+        "game_id": "doapp",
+        "env_address": "0.0.0.0:50052",
+        "player": "Random",
+        "step_ratio": 6,
+        "frame_shape": [128, 128, 1],
+        "hardcore": False,
+        "difficulty": 4,
+        "characters": [["Kasumi"], ["Kasumi"]],
+        "char_outfits": [2, 2],
+        "action_space": "discrete",
+        "attack_but_combination": False
+    }
 
-# Additional obs key list
-key_to_add = []
-key_to_add.append("actions")
+    # Wrappers Settings
+    wrappers_settings = {
+        "reward_normalization": True,
+        "frame_stack": 4,
+        "actions_stack": 12,
+        "scale": True
+    }
 
-key_to_add.append("ownHealth")
-key_to_add.append("oppHealth")
+    # Additional obs key list
+    key_to_add = []
+    key_to_add.append("actions")
 
-key_to_add.append("ownSide")
-key_to_add.append("oppSide")
-key_to_add.append("stage")
+    key_to_add.append("ownHealth")
+    key_to_add.append("oppHealth")
 
-env, num_env = make_stable_baselines_env(time_dep_seed, settings, wrappers_settings,
-                                         key_to_add=key_to_add, use_subprocess=False)
+    key_to_add.append("ownSide")
+    key_to_add.append("oppSide")
+    key_to_add.append("stage")
 
-# Load the trained agent
-model = PPO2.load(os.path.join(model_folder, "doappSmall25M"))
+    env, num_env = make_stable_baselines_env(time_dep_seed, settings, wrappers_settings,
+                                            key_to_add=key_to_add, use_subprocess=False)
 
-obs = env.reset()
+    # Load the trained agent
+    model = PPO2.load(os.path.join(model_folder, "doappSmall25M"))
 
-while True:
+    obs = env.reset()
 
-    action, _ = model.predict(obs, deterministic=True)
+    while True:
 
-    obs, reward, done, info = env.step(action)
+        action, _ = model.predict(obs, deterministic=True)
 
-    if done:
-        pass  # No need to call reset method since it is done by the vectorized environment
+        obs, reward, done, info = env.step(action)
 
-# Close the environment
-env.close()
+        if done:
+            pass  # No need to call reset method since it is done by the vectorized environment
+
+    # Close the environment
+    env.close()
