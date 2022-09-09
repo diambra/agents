32995704859d3cb8f5827405c0cd2d721615f63b
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2022-09-09 15:48:15 +0200

    Updated basic example

--- stable_baselines/v3/examples/basic.py
+++ stable_baselines/v3/examples/basic.py
@@ -5,7 +5,7 @@ env = diambra.arena.make("doapp", {"hardcore": True, "frame_shape": [128, 128, 1
 
 print("\nStarting training ...\n")
 model = A2C('CnnPolicy', env, verbose=1)
-model.learn(total_timesteps=1000)
+model.learn(total_timesteps=200)
 print("\n .. training completed.")
 
 print("\nStarting evaluation ...\n")
