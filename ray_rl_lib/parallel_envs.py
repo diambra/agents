d9e646bf56cc906138d9bb8d476000f79a80479c
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2022-09-27 18:03:24 +0200

    Added parallel envs example

---
+++ ray_rl_lib/parallel_envs.py
@@ -0,0 +1,60 @@
+import sys
+import os
+import diambra.arena
+from make_ray_env import DiambraArena, preprocess_ray_config
+from ray.rllib.algorithms.ppo import PPO
+from ray.tune.logger import pretty_print
+
+if __name__ == "__main__":
+
+    # Settings
+    settings = {}
+    settings["hardcore"] = True
+    settings["frame_shape"] = [84, 84, 3]
+
+    config = {
+        # Define and configure the environment
+        "env": DiambraArena,
+        "env_config": {
+            "game_id": "doapp",
+            "settings": settings,
+        },
+
+        "train_batch_size": 200,
+
+        # Use 2 rollout workers
+        "num_workers": 2,
+        # Use a vectorized env with 2 sub-envs.
+        "num_envs_per_worker": 2,
+
+        # Evaluate once per training iteration.
+        "evaluation_interval": 1,
+        # Run evaluation on (at least) two episodes
+        "evaluation_duration": 2,
+        # ... using one evaluation worker (setting this to 0 will cause
+        # evaluation to run on the local evaluation worker, blocking
+        # training until evaluation is done).
+        "evaluation_num_workers": 1,
+        # Special evaluation config. Keys specified here will override
+        # the same keys in the main config, but only for evaluation.
+        "evaluation_config": {
+            # Render the env while evaluating.
+            # Note that this will always only render the 1st RolloutWorker's
+            # env and only the 1st sub-env in a vectorized env.
+            "render_env": True,
+        },
+    }
+
+    # Update config file
+    config = preprocess_ray_config(config)
+
+    # Create the RLlib Agent.
+    agent = PPO(config=config)
+
+    # Run it for n training iterations
+    print("\nStarting training ...\n")
+    for idx in range(2):
+        print("Training iteration:", idx + 1)
+        results = agent.train()
+    print("\n .. training completed.")
+    print("Training results:\n{}".format(pretty_print(results)))
