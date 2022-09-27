a9b410d8f77a5f2bdeeda2b4dec65a8208d7e1d0
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2022-09-27 15:14:21 +0200

    Working basic example

--- ray_rl_lib/make_ray_env.py
+++ ray_rl_lib/make_ray_env.py
@@ -10,7 +10,7 @@ class DiambraArena(gym.Env):
 
     def __init__(self, config: EnvContext):
 
-        logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger(__name__)
 
         if "is_rollout" not in config.keys():
             message = "Environment initialized without a preprocessed config file."
@@ -30,6 +30,14 @@ class DiambraArena(gym.Env):
         create_env_on_driver = config["create_env_on_driver"]
         is_rollout = config["is_rollout"]
 
+        self.logger.debug("num_rollout_workers: {}".format(num_rollout_workers))
+        self.logger.debug("num_eval_workers: {}".format(num_eval_workers))
+        self.logger.debug("num_envs_per_worker: {}".format(num_envs_per_worker))
+        self.logger.debug("worker_index: {}".format(worker_index))
+        self.logger.debug("vector_index: {}".format(vector_index))
+        self.logger.debug("create_env_on_driver: {}".format(create_env_on_driver))
+        self.logger.debug("is_rollout: {}".format(is_rollout))
+
         if is_rollout is True:
             self.rank = vector_index + (worker_index - 1) * num_envs_per_worker
         else:
@@ -38,7 +46,7 @@ class DiambraArena(gym.Env):
         if create_env_on_driver is True:
             self.rank += num_envs_per_worker
 
-        print("RANK = ", self.rank)
+        self.logger.debug("Rank: {}".format(self.rank))
 
         self.env = diambra.arena.make(self.game_id, self.settings, self.wrappers_settings,
                                       seed=self.seed + self.rank, rank=self.rank)
@@ -52,6 +60,9 @@ class DiambraArena(gym.Env):
     def step(self, action):
         return self.env.step(action)
 
+    def render(self):
+        return self.env.render()
+
 def preprocess_ray_config(config):
 
     logger = logging.getLogger(__name__)
@@ -59,8 +70,13 @@ def preprocess_ray_config(config):
     num_envs_required = 0
     num_envs_per_worker = config["num_envs_per_worker"] if "num_envs_per_worker" in config.keys() else 1
 
+    if "num_workers" not in config.keys():
+        config["num_workers"] = 2
+    if "create_env_on_driver" not in config.keys():
+        config["create_env_on_driver"] = False
+
     # Set create_env_on_driver appropriately
-    if ("num_workers" not in config.keys() or config["num_workers"] == 0) or\
+    if (config["num_workers"] == 0) or\
        (("evaluation_interval" in config.keys() and config["evaluation_interval"] is not None and config["evaluation_interval"] > 0) and\
         ("evaluation_num_workers" not in config.keys() or ("evaluation_num_workers" in config.keys() and config["evaluation_num_workers"] == 0))) :
         config["create_env_on_driver"] = True
@@ -70,7 +86,7 @@ def preprocess_ray_config(config):
         num_envs_required += num_envs_per_worker
 
     # Define rollout workers related variables
-    num_workers = config["num_workers"] if "num_workers" in config.keys() else 0
+    num_workers = config["num_workers"]
     # Add rollout workers
     num_envs_required += num_workers*num_envs_per_worker
 
@@ -79,8 +95,7 @@ def preprocess_ray_config(config):
     # Add evaluation workers
     num_envs_required += evaluation_num_workers*num_envs_per_worker
 
-    #logger.debug("Total number of environments required = {}".format(num_envs_required))
-    print("Total number of environments required = {}".format(num_envs_required))
+    logger.debug("Total number of environments required = {}".format(num_envs_required))
 
     if num_envs_required > diambra.arena.get_num_envs():
         message = "Config file requires more environments than"
@@ -93,7 +108,7 @@ def preprocess_ray_config(config):
         message += " To avoid wasting resources, use the CLI option '-s={}' for this configuration.".format(num_envs_required)
         logger.warning(message)
 
-    config["env_config"]["create_env_on_driver"] = config["create_env_on_driver"] if "create_env_on_driver" in config.keys() else False
+    config["env_config"]["create_env_on_driver"] = config["create_env_on_driver"]
 
     # Setting flag to let the env class know if it is an evaluation env or not
     config["env_config"]["is_rollout"] = True
@@ -110,38 +125,3 @@ def preprocess_ray_config(config):
 
     return config
 
-'''
-# === Settings for Rollout Worker processes ===
-# Number of rollout worker actors to create for parallel sampling. Setting
-# this to 0 will force rollouts to be done in the algorithm's actor.
-"num_workers": 2,
-# Number of environments to evaluate vector-wise per worker. This enables
-# model inference batching, which can improve performance for inference
-# bottlenecked workloads.
-"num_envs_per_worker": 1,
-# When `num_workers` > 0, the driver (local_worker; worker-idx=0) does not
-# need an environment. This is because it doesn't have to sample (done by
-# remote_workers; worker_indices > 0) nor evaluate (done by evaluation
-# workers; see below).
-"create_env_on_driver": False,
-
-# Whether to use "rllib" or "deepmind" preprocessors by default
-# Set to None for using no preprocessor. In this case, the model will have
-# to handle possibly complex observations from the environment.
-"preprocessor_pref": "deepmind",
-
-# === Evaluation Settings ===
-# Evaluate with every `evaluation_interval` training iterations.
-# The evaluation stats will be reported under the "evaluation" metric key.
-# Note that for Ape-X metrics are already only reported for the lowest
-# epsilon workers (least random workers).
-# Set to None (or 0) for no evaluation.
-"evaluation_interval": None,
-# Number of parallel workers to use for evaluation. Note that this is set
-# to zero by default, which means evaluation will be run in the algorithm
-# process (only if evaluation_interval is not None). If you increase this,
-# it will increase the Ray resource usage of the algorithm since evaluation
-# workers are created separately from rollout workers (used to sample data
-# for training).
-"evaluation_num_workers": 0,
-'''
