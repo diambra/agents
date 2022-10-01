9c3d7e310cd7d541ed36f415d5d9acbf151bf3f5
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2022-10-01 20:02:38 +0200

    Updating sb2

--- stable_baselines/evaluation.py
+++ stable_baselines/evaluation.py
@@ -4,10 +4,7 @@ import time
 import yaml
 import json
 import argparse
-base_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(base_path, '../'))
-from make_sb_env import make_sb_env
-
+from diambra.arena.stable_baselines.make_sb_env import make_sb_env
 from stable_baselines import PPO2
 
 # diambra run -g python stable_baselines/v2/scripts/evaluation.py --cfgFile $PWD/stable_baselines/v2/scripts/cfg_files/doapp/sr6_128x4_das_nc.yaml  --trainedModel "25M"
@@ -29,6 +26,7 @@ if __name__ == '__main__':
 
         time_dep_seed = int((time.time() - int(time.time() - 0.5)) * 1000)
 
+        base_path = os.path.dirname(os.path.abspath(__file__))
         model_folder = os.path.join(base_path, params["folders"]["parent_dir"], params["settings"]["game_id"],
                                     params["folders"]["model_name"], "model")
 
