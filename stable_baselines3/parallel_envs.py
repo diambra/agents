ec4ef10b6b6976d0e2ce2f6a82f605f84be3440e
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2022-10-01 19:02:31 +0200

    Updated sb3 and working

--- stable_baselines3/parallel_envs.py
+++ stable_baselines3/parallel_envs.py
@@ -1,50 +1,8 @@
 import os
 import sys
 import diambra.arena
-
+from diambra.arena.stable_baselines3.make_sb3_env import make_sb3_env
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecFrameStack
-from stable_baselines3.common.utils import set_random_seed
-
-# Make Stable Baselines Env function
-def make_sb3_env(game_id, env_settings, wrappers_settings=None,
-                 use_subprocess=True, seed=0):
-    """
-    Create a wrapped VecEnv.
-    :param game_id: (str) the game environment ID
-    :param env_settings: (dict) parameters for DIAMBRA Arena environment
-    :param wrappers_settings: (dict) parameters for environment
-                              wraping function
-    :param use_subprocess: (bool) Whether to use `SubprocVecEnv` or
-                          `DummyVecEnv` when
-    :param no_vec: (bool) Whether to avoid usage of Vectorized Env or not.
-                   Default: False
-    :param seed: (int) initial seed for RNG
-    :return: (VecEnv) The diambra environment
-    """
-
-    env_addresses = os.getenv("DIAMBRA_ENVS", "").split()
-    if len(env_addresses) == 0:
-        raise Exception("ERROR: Running script without DIAMBRA CLI.")
-        sys.exit(1)
-
-    num_envs = len(env_addresses)
-
-    def make_sb_env(rank):
-        def _init():
-            env = diambra.arena.make(game_id, env_settings, wrappers_settings,
-                                     seed=seed + rank, rank=rank)
-            return env
-        return _init
-    set_random_seed(seed)
-
-    # When using one environment, no need to start subprocesses
-    if num_envs == 1 or not use_subprocess:
-        env = DummyVecEnv([make_sb_env(i) for i in range(num_envs)])
-    else:
-        env = SubprocVecEnv([make_sb_env(i) for i in range(num_envs)])
-
-    return env, num_envs
 
 if __name__ == '__main__':
 
--- stable_baselines3/saving_loading_evaluating.py
+++ stable_baselines3/saving_loading_evaluating.py
@@ -2,45 +2,47 @@ import diambra.arena
 from stable_baselines3 import A2C
 from stable_baselines3.common.evaluation import evaluate_policy
 
-# Create environment
-env = diambra.arena.make("doapp", {"hardcore": True, "frame_shape": [128, 128, 1]})
+if __name__ == "__main__":
 
-# Instantiate the agent
-agent = A2C('CnnPolicy', env, verbose=1)
-# Train the agent
-agent.learn(total_timesteps=200)
-# Save the agent
-agent.save("a2c_doapp")
-del agent  # delete trained agent to demonstrate loading
+    # Create environment
+    env = diambra.arena.make("doapp", {"hardcore": True, "frame_shape": [128, 128, 1]})
 
-# Load the trained agent
-# NOTE: if you have loading issue, you can pass `print_system_info=True`
-# to compare the system on which the agent was trained vs the current one
-# agent = A2C.load("a2c_doapp", env=env, print_system_info=True)
-agent = A2C.load("a2c_doapp", env=env)
+    # Instantiate the agent
+    agent = A2C('CnnPolicy', env, verbose=1)
+    # Train the agent
+    agent.learn(total_timesteps=200)
+    # Save the agent
+    agent.save("a2c_doapp")
+    del agent  # delete trained agent to demonstrate loading
 
-# Evaluate the agent
-# NOTE: If you use wrappers with your environment that modify rewards,
-#       this will be reflected here. To evaluate with original rewards,
-#       wrap environment in a "Monitor" wrapper before other wrappers.
-mean_reward, std_reward = evaluate_policy(agent, agent.get_env(), n_eval_episodes=3)
-print("Reward: {} (avg) ± {} (std)".format(mean_reward, std_reward))
+    # Load the trained agent
+    # NOTE: if you have loading issue, you can pass `print_system_info=True`
+    # to compare the system on which the agent was trained vs the current one
+    # agent = A2C.load("a2c_doapp", env=env, print_system_info=True)
+    agent = A2C.load("a2c_doapp", env=env)
 
-# Run trained agent
-observation = env.reset()
-cumulative_reward = 0
-while True:
-    env.render()
+    # Evaluate the agent
+    # NOTE: If you use wrappers with your environment that modify rewards,
+    #       this will be reflected here. To evaluate with original rewards,
+    #       wrap environment in a "Monitor" wrapper before other wrappers.
+    mean_reward, std_reward = evaluate_policy(agent, agent.get_env(), n_eval_episodes=3)
+    print("Reward: {} (avg) ± {} (std)".format(mean_reward, std_reward))
 
-    action, _state = agent.predict(observation, deterministic=True)
+    # Run trained agent
+    observation = env.reset()
+    cumulative_reward = 0
+    while True:
+        env.render()
 
-    observation, reward, done, info = env.step(action)
-    cumulative_reward += reward
-    if (reward != 0):
-        print("Cumulative reward =", cumulative_reward)
+        action, _state = agent.predict(observation, deterministic=True)
 
-    if done:
-        observation = env.reset()
-        break
+        observation, reward, done, info = env.step(action)
+        cumulative_reward += reward
+        if (reward != 0):
+            print("Cumulative reward =", cumulative_reward)
 
-env.close()
+        if done:
+            observation = env.reset()
+            break
+
+    env.close()
