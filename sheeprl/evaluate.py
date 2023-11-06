3490393bb0f5a7a6587f725b527ab0f720ba1703
Michele Milesi <michele.milesi@studio.unibo.it>
2023-11-06 17:07:19 +0100

    feat: added sheeprl examples

---
+++ sheeprl/evaluate.py
@@ -0,0 +1,16 @@
+# Diambra Agents
+
+import hydra
+from diambra.arena.sheeprl import CONFIGS_PATH
+from omegaconf import DictConfig
+
+from sheeprl.cli import evaluation
+
+
+@hydra.main(version_base="1.13", config_path=CONFIGS_PATH, config_name="eval_config")
+def run(cfg: DictConfig):
+    evaluation(cfg)
+
+
+if __name__ == "__main__":
+    run()
