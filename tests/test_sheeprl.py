cb31a6ab339f12f9e79fe7fd5f5520d37d9656f1
Michele Milesi <michele.milesi@studio.unibo.it>
2023-11-16 17:15:54 +0100

    tests: added tests for sheeprl

---
+++ tests/test_sheeprl.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+import os
+import shutil
+import sys
+import warnings
+from unittest import mock
+
+import pytest
+from diambra.arena.utils.engine_mock import load_mocker
+
+# Add the scripts directory to sys.path
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "sheeprl"))
+sys.path.append(ROOT_DIR)
+
+import evaluate
+import train
+
+STANDARD_ARGS = [
+    os.path.join(ROOT_DIR, "__main__.py"),
+    "env.capture_video=False",
+    "metric.log_level=0",
+    "checkpoint.every=10000000",
+    "buffer.memmap=False",
+]
+
+
+def func(mocker, n_envs, args, evaluation=False, root_dir=None, run_name=None):
+    load_mocker(mocker)
+
+    try:
+        # Environment setup
+        initial_port = 32781
+        envs = f"127.0.0.1:{initial_port}"
+        for i in range(1, n_envs):
+            envs += f" 127.0.0.1:{initial_port + i}"
+        os.environ["DIAMBRA_ENVS"] = envs
+
+        # SheepRL config folder setup
+        os.environ[
+            "SHEEPRL_SEARCH_PATH"
+        ] = "file://sheeprl/configs;pkg://sheeprl.configs"
+
+        # Execution of the train script
+        with mock.patch.object(sys, "argv", STANDARD_ARGS + args):
+            train.train()
+
+        if evaluation:
+            # Take checkpoint
+            ckpt_root = os.path.join("logs", "runs", root_dir, run_name)
+            ckpt_dir = sorted([d for d in os.listdir(ckpt_root) if "version" in d])[-1]
+            ckpt_path = os.path.join(ckpt_root, ckpt_dir, "checkpoint")
+            ckpt_file_name = os.listdir(ckpt_path)[-1]
+            ckpt_path = os.path.join(ckpt_path, ckpt_file_name)
+            # Execution of the evaluate script
+            with mock.patch.object(
+                sys, "argv", STANDARD_ARGS[:1] + [f"checkpoint_path={ckpt_path}"]
+            ):
+                evaluate.run()
+
+        # Delete log folder
+        try:
+            shutil.rmtree("./logs", False, None)
+        except (OSError, WindowsError):
+            warnings.warn("Unable to delete folder {}.".format("./logs"))
+        return 0
+    except Exception as e:
+        print(e)
+        return 1
+
+
+def test_sheeprl_train_base(mocker):
+    assert (
+        func(
+            mocker,
+            2,
+            ["exp=custom_exp", "checkpoint.save_last=False"],
+        )
+        == 0
+    )
+
+
+def test_sheeprl_train_parallel_envs(mocker):
+    assert (
+        func(
+            mocker,
+            6,
+            ["exp=custom_parallel_env_exp", "checkpoint.save_last=False"],
+        )
+        == 0
+    )
+
+
+def test_sheeprl_train_fabric(mocker):
+    assert (
+        func(
+            mocker,
+            2,
+            [
+                "exp=custom_fabric_exp",
+                "fabric.accelerator=cpu",
+                "fabric.devices=1",
+                "checkpoint.save_last=False",
+            ],
+        )
+        == 0
+    )
+
+
+def test_sheeprl_train_metrics(mocker):
+    assert (
+        func(
+            mocker,
+            2,
+            [
+                "exp=custom_metric_exp",
+                "fabric.accelerator=cpu",
+                "fabric.devices=1",
+                "checkpoint.save_last=False",
+            ],
+        )
+        == 0
+    )
+
+
+def test_sheeprl_evaluation(mocker):
+    assert (
+        func(
+            mocker,
+            3,
+            [
+                "exp=custom_exp",
+                "checkpoint.save_last=True",
+                "root_dir=pytest_ppo",
+                "run_name=eval",
+            ],
+            evaluation=True,
+            root_dir="pytest_ppo",
+            run_name="eval",
+        )
+        == 0
+    )
